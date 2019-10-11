0bb4d68524835f4191245994050a1c797049a429
Debabratapal <paldebabrata82@gmail.com>
2019-10-11 14:45:26 +0530

    Updates

---
+++ precache-manifest.04bc5b316b4ee2dcb9d2f1b42a78736e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6ae30d0fc3c79dfa8296f0c2c2e56d",
+    "url": "/helloui/index.html"
+  },
+  {
+    "revision": "4c2052f2ee9970f3a251",
+    "url": "/helloui/static/css/main.db3c4536.chunk.css"
+  },
+  {
+    "revision": "97ab2796eda88a61abc5",
+    "url": "/helloui/static/js/2.f5718bce.chunk.js"
+  },
+  {
+    "revision": "4c2052f2ee9970f3a251",
+    "url": "/helloui/static/js/main.93b8a8af.chunk.js"
+  },
+  {
+    "revision": "e1e1b0d3c9b610342166",
+    "url": "/helloui/static/js/runtime-main.cc2bffa8.js"
+  },
+  {
+    "revision": "fa70efe542cbbf702ad2adb5563e9e33",
+    "url": "/helloui/static/media/1.fa70efe5.svg"
+  },
+  {
+    "revision": "18a63a2d66f3c91c5cc2f53fa22735de",
+    "url": "/helloui/static/media/10.18a63a2d.svg"
+  },
+  {
+    "revision": "90a67cfa27c6ef1a641b57be42092e6d",
+    "url": "/helloui/static/media/2.90a67cfa.svg"
+  },
+  {
+    "revision": "f08c79904408a6471f1e9dc70206d777",
+    "url": "/helloui/static/media/3.f08c7990.svg"
+  },
+  {
+    "revision": "749ed7583d208c710a1a10a5d0df5f62",
+    "url": "/helloui/static/media/4.749ed758.svg"
+  },
+  {
+    "revision": "de99b10bbf5ab613148714a8e0f60c1e",
+    "url": "/helloui/static/media/5.de99b10b.svg"
+  },
+  {
+    "revision": "b2838127791c1a34aea6ef3accf98e9b",
+    "url": "/helloui/static/media/6.b2838127.svg"
+  },
+  {
+    "revision": "29d5e236aeccf9334934c428423a9b24",
+    "url": "/helloui/static/media/7.29d5e236.svg"
+  },
+  {
+    "revision": "797c8b64b09f683246e39080cbb1d535",
+    "url": "/helloui/static/media/8.797c8b64.svg"
+  },
+  {
+    "revision": "fb65a652368e3eed015510b048e80b6c",
+    "url": "/helloui/static/media/9.fb65a652.svg"
+  }
+]);
